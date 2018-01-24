9ace5e8d576e15d94572293c03a66691b4312c6c
Matt Sweeney <matt@savvior.com>
2018-01-24 16:10:17 -0500

    Add basic UI and click events

---
+++ Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEvent : MonoBehaviour {
+
+    public UnityEngine.UI.Text woodText;
+    public int wood = 0;
+    public int woodclick = 1;
+
+    void Update() {
+        woodText.text = "Wood: " + wood;
+    }
+    public void clicked() {
+        wood += woodclick;
+    }
+
+}
